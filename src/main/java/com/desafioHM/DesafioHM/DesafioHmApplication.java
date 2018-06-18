c33c4a710174b7f8f35a1d5cba58cea870a63b9c
Felipe Oliveira <Fel_oliveira_silva@yahoo.com.br>
2018-06-17 23:27:16 -0300

    Criando estrutura inicial do spring boot com gradle

---
+++ src/main/java/com/desafioHM/DesafioHM/DesafioHmApplication.java
@@ -0,0 +1,12 @@
+package com.desafioHM.DesafioHM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioHmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioHmApplication.class, args);
+	}
+}
